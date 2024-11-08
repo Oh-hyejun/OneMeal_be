6b68124680ceb4fc6942e158d8c29d86a4d04e76
Oh-hyejun <qwegw0303@naver.com>
2024-11-08 17:04:20 +0900

    first commit

---
+++ src/test/java/com/OneMeal/OneMeal_be/OneMealBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OneMeal.OneMeal_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneMealBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
