15d727b93d77b9e1beed6d2dd482b9fb78089c02
오혜준 <qwegw0303@naver.com>
2024-11-09 17:13:05 +0900

    rest api modify

--- src/main/java/com/OneMeal/OneMeal_be/FrontController.java
+++ src/main/java/com/OneMeal/OneMeal_be/FrontController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000")
 public class FrontController {
 
     @GetMapping("/api/test")
