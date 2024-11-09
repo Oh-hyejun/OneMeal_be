bc73274534e8d66f07e5ec495859505ca5d444e6
오혜준 <qwegw0303@naver.com>
2024-11-09 15:49:54 +0900

    test file

--- src/main/java/com/OneMeal/OneMeal_be/TestController.java
+++ src/main/java/com/OneMeal/OneMeal_be/TestController.java
@@ -10,4 +10,9 @@ public class TestController {
     public String home() {
         return "home.html";
     }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login.html";
+    }
 }
