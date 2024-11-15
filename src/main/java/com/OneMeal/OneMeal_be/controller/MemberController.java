5f249a4ee357ed4e0cf308b2a5709f0615813d0d
오혜준 <qwegw0303@naver.com>
2024-11-15 16:10:23 +0900

    modify RestController

--- src/main/java/com/OneMeal/OneMeal_be/controller/MemberController.java
+++ src/main/java/com/OneMeal/OneMeal_be/controller/MemberController.java
@@ -3,11 +3,13 @@ package com.OneMeal.OneMeal_be.controller;
 import com.OneMeal.OneMeal_be.dto.MemberDTO;
 import com.OneMeal.OneMeal_be.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class MemberController {
 
